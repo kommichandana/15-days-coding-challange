52452f75e64fb4c8b3bc6950b773f8f8a723f13d
kommichandana <154341985+kommichandana@users.noreply.github.com>
2025-08-05 23:53:35 +0530

    Time: 14 ms (31.6%), Space: 45.3 MB (16.4%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String reversed="";
-        for(int i=s.length();i>=0;i++)
+       s = s.toLowerCase().replaceAll("[^A-Za-z0-9]", "");
+        int i=0;
+        int j=s.length()-1;
+        while(i<=j)
         {
-            reversed=reversed+s.charAt(i);
+            if(s.charAt(i)!=s.charAt(j))
+            {
+                return false;
+            }
+            i++;
+            j--;
         }
-        if(reversed.equals(s))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        return true;
     }
 }
